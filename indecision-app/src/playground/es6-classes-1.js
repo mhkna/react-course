b34afc20d4f47f1ef3dc860bbaf3fea7390166e7
Michael <michkna3@gmail.com>
2017-10-25 13:46:46 -0500

    add traveler and student subclasses to show super and overriding functions in es6

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -9,12 +9,50 @@ class Person {
     return `Hi. I am ${this.name}!`
   }
   getDescription() {
-    return `${this.name} is ${this.age} year(s) old`
+    return `${this.name} is ${this.age} year(s) old.`
   }
 }
 
-const me = new Person('Michael Knapp', 27);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  //override parent function above
+  getDescription() {
+    //set parent function to var...remember to pass in arguments (but here there are none)
+    let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+    if (this.hasMajor()) {
+        // description = description + ` Their major is ${this.major}.`
+        description += ` Their major is ${this.major}.`
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+      super(name, age);
+      this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+      let greeting = super.getGreeting();
+
+      if (this.homeLocation) {
+        greeting += ` I'm visiting from ${this.homeLocation}.`
+      }
+
+      return greeting;
+    }
+}
+
+const me = new Traveler('Michael Knapp', 27, 'Detroit');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
