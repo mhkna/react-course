d2ef4cafa5311fef81f2420f76cb5a40844fd469
Michael <michkna3@gmail.com>
2017-10-25 16:54:57 -0500

    in counter-example we've now added state and using prevstate to increment counter

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -4,20 +4,38 @@ class Counter extends React.Component {
     this.handleAddOne = this.handleAddOne.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    // pieces of state we want to track... here only count
+    this.state = {
+      count: 0,
+      name: 'Michael'
+    };
   }
   handleAddOne() {
-    console.log('handleAddOne');
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      };
+    });
   }
   handleMinusOne() {
-    console.log('handleMinusOne');
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      };
+    });
   }
   handleReset() {
-    console.log('handleReset');
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
   }
   render() {
     return (
       <div>
-        <h1>Count: </h1>
+        {this.state.name}
+        <h1>Count: {this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
