5f373b8bf1a064326e4209516995afcee7b3ed2f
Michael <michkna3@gmail.com>
2017-10-25 14:07:24 -0500

    added header and action react components that can then be passed to the jsx

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,31 @@
+// react component is just an es6 class
+class Header extends React.Component {
+  // react components MUST define render
+  render() {
+    return (
+      <div>
+        <h1>Indeicison</h1>
+        <h2>We will choose for you</h2>
+      </div>
+    );
+  }
+}
 
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+const jsx = (
+  <div>
+    <Header/>
+    <Action/>
+  </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
