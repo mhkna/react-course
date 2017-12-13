f2fa52d9e32ad8dba0d0eb91269c12d1eb5879c5
Michael <michkna3@gmail.com>
2017-12-12 19:04:08 -0600

    initial client side routing using reareact-router

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+const ExpenseDashboardPage = () => (
+  <div>
+    From my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    From my Add Expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    EDIT
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+HELP
+  </div>
+);
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'))
