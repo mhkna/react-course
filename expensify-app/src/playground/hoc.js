d177cd5eb6d8b9e567e96804e50b5f66a2f19763
Michael <michkna3@gmail.com>
2017-12-15 19:44:10 -0600

    react higher order components, react with redux, use of connect, and sorting expenses by either date or amount using select

---
+++ expensify-app/src/playground/hoc.js
@@ -0,0 +1,42 @@
+// Higher Order Component (HOC) - A component (HOC) that renders another component
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAdmin && <p>This is private info. Please don't shave</p> }
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+        <p>Login to view info</p>
+      )}
+    </div>
+  );
+};
+
+const AuthInfo = requireAuthentication(Info)
+const AdminInfo = withAdminWarning(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="These are the details" />, document.getElementById('app'));
